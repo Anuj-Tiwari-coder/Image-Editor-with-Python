93588b254366f62ef3579b7a86862d7e13bd7c67
Anuj Tiwari <anujtiwari0043@gmail.com>
2025-01-20 18:03:22 +0530

    Add files via upload

---
+++ Image editer/mini projects/ImageProcesser.py
@@ -0,0 +1,24 @@
+from PIL import Image, ImageEnhance, ImageFilter
+import os
+
+
+path = './imgs'
+PathOut = './editedImgs'
+
+if not os.path.exists(path):
+    os.makedirs(path)
+    print(f"Created missing directory: {path}. Add images to this folder and rerun the script.")
+
+os.makedirs(PathOut, exist_ok=True)
+
+for filename in os.listdir(path):
+    if filename.endswith(('.png', '.jpg', '.jpeg')):
+        img = Image.open(f"{path}/{filename}")
+        
+        factor = 1.5
+        edit = img.filter(ImageFilter.SHARPEN)
+        enhancer = ImageEnhance.Brightness(edit)
+        edit = enhancer.enhance(factor)
+        
+        clean_name = os.path.splitext(filename)[0]
+        edit.save(f"{PathOut}/{clean_name}_edited.jpg")
